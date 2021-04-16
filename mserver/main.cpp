c5a2d8b0b455494975da8798494b3b0060ba4277
Jose Retana <joseantonioretanacorrales@gmail.com>
2021-04-16 01:06:49 -0600

    mserver: inicio del manejo de sockets
    Se comienza la configuración básica de los sockets para la comunicación entre el mserver y c!

--- mserver/main.cpp
+++ mserver/main.cpp
@@ -1,8 +1,79 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+using namespace std;
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+char findSocket(){
+
+}
+
+int baseSocketNumber = 5000;
+void bindingProcess(int* sockfd, int* portno, struct sockaddr_in* serv_addr){
+    *portno = baseSocketNumber;
+    serv_addr->sin_family = AF_UNIX;
+    serv_addr->sin_port = htons(*portno);
+    serv_addr->sin_addr.s_addr = INADDR_ANY;
+
+    cout << "Binding to port " << *portno << "..." << endl;
+    if (bind(*sockfd, (struct sockaddr *) serv_addr , sizeof(*serv_addr)) < 0){
+        cout << "Error on Binding: Address already in use" << endl;
+        baseSocketNumber += 1;
+        bindingProcess(sockfd, portno, serv_addr);
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    int sockfd, newsockfd, portno, n;
+    int option = 1;
+    socklen_t clilen;
+    char buffer[255];
+    struct sockaddr_in serv_addr, cli_addr;
+
+    cout << "Opening Socket..." << endl;
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEPORT,&option,sizeof(int)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
+    if (sockfd < 0){
+        error("ERROR opening socket");
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+
+    bindingProcess(&sockfd, &portno, &serv_addr);
+
+
+    listen(sockfd,5);
+    clilen = sizeof(cli_addr);
+
+    cout << "Waiting for c! client..." << endl;
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+    if (newsockfd < 0) {
+        error("ERROR on accept");
+    }
+
+    void* startAdress = malloc(10000);
+
+    cout << startAdress << endl;
+
+    free(startAdress);
 
     return a.exec();
 }
